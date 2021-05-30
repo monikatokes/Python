1a29174c8d77ead35feca1509a9cedefd0e5ff91
monikatokes <monika.tokes@tss-yonder.com>
2021-05-30 17:45:44 +0300

    Tema 5

---
+++ Tema5/card.py
@@ -0,0 +1,60 @@
+import random
+
+
+class Card:
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def display_card(self):
+        return self.value + ' of ' + self.suit
+
+    def get_card_suit(self):
+        return self.suit
+
+    def get_card_value(self):
+        return self.value
+
+
+class Deck:
+
+    def __init__(self):
+        # create deck
+        value_list = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+        suit_list = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+        cards_list = []
+        for i in value_list:
+            for j in suit_list:
+                cards_list.append(Card(j, i))
+        print(cards_list.__len__())
+        self.cards_list = cards_list
+
+    def deal_remove_card(self, suit, value):
+
+        found = 0
+        for i in range(self.cards_list.__len__()):
+            if self.cards_list[i].get_card_suit() == suit:
+                if self.cards_list[i].get_card_value() == value:
+                    self.cards_list.remove(self.cards_list[i])
+                    found = 1
+                    break
+        if found == 0:
+            raise Exception("Card was not found")
+
+    def print_elements(self):
+        for i in range(self.cards_list.__len__()):
+            print(self.cards_list[i].display_card())
+
+    def shuffle_cards(self):
+        random.shuffle(self.cards_list)
+        if self.cards_list.__len__() != 52:
+            raise Exception("Cards cannot be shuffled")
+
+
+deck = Deck()
+print(deck.cards_list.__len__())
+deck.deal_remove_card('Diamonds', 'A')
+print(deck.cards_list.__len__())
+deck.shuffle_cards()
+deck.print_elements()
